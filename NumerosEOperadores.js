d1daccd68dfd7aa2f8a856a6b4a7c2def9dcb06c
Nicolas Yuri <79637247+YuriNick-jpg@users.noreply.github.com>
2023-03-02 16:08:51 -0300

    Add files via upload

---
+++ NumerosEOperadores.js
@@ -0,0 +1,25 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+// Crie duas expressões que retornem NaN
+var peso = 'peso 14' - 10
+console.log(peso)
+var salario = '14 reais' - 10
+console.log(salario)
+
+// Somar a string '200' com o número 50 e retornar 250
+var n = parseInt('200') + 50
+console.log(n)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var valor = 10
+var incremento = +5
+var valor2 = valor + incremento
+console.log(valor2)
+
+// Como dividir o peso por 2?
+var peso = 80
+var unidade = 'kg'
+var metadePeso = (peso / 2) + unidade
+console.log(peso + unidade)
+console.log(metadePeso)
---
+++ TiposDeDados.js
@@ -0,0 +1,33 @@
+// Declare uma variável contendo uma string
+var nome = 'nicolas'
+console.log(nome)
+console.log(typeof nome)
+
+// Declare uma variável contendo um número dentro de uma string
+var anos = 24
+var idade = `${anos} anos` // Template string
+console.log(idade)
+console.log(typeof idade)
+// Declare uma variável com a sua idade
+var Idade = 24
+console.log(Idade)
+console.log(typeof Idade)
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Nicolas'
+var sobreNome = ' Yuri'
+var nomeCompleto = nome + sobreNome
+console.log(nomeCompleto)
+console.log(typeof nomeCompleto)
+
+// Coloque a seguinte frase em uma variável: It's time
+var doce = "It's time"
+console.log(doce)
+console.log(typeof doce)
+
+// Verifique o tipo da variável que contém o seu nome
+var verificaTipoNome = typeof nome;
+console.log(verificaTipoNome)
+
+// apagar
