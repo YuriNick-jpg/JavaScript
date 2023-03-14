72b55953f833f3103fcd262ecfaad126e215800d
Nicolas Yuri <79637247+YuriNick-jpg@users.noreply.github.com>
2023-03-14 14:01:02 -0300

    Add files via upload

---
+++ manipulandoDOM.js
@@ -0,0 +1,22 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlSite = window.location
+ console.log(`a url do site é , ${urlSite}`) 
+
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const classes = document.querySelector(".ativo");
+console.log(classes)
+
+
+
+// Retorne a linguagem do navega
+
+const linguagem = navigator.language
+console.log(linguagem)
+
+// Retorne a largura da janela
+
+const largura = window.innerWidth
+console.log(largura)
